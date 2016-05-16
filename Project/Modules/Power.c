e25ac77e0a153ccaad8ab9dc304a761c6219c7c9
ProkopenkoRoman <long399@mail.ru>
2016-05-16 22:31:05 +0600

    new version

--- Project/Modules/Power.c
+++ Project/Modules/Power.c
@@ -1,435 +1,272 @@
 #include <stdio.h>
-
-/* ФУНКЦИИ КОНВЕРТИРОВАНИЯ РАЗЛИЧНЫХ ЕДИНИЦ МОЩНОСТИ */
-
-// <Ваты> ---> <Киловаты> //
-double Vat2kVat(double POWER) {
-	POWER = POWER / 1000;
-	return POWER;
-}
-
-// <Киловаты> ---> <Ваты> //
-double kVat2Vat(double POWER) {
-	POWER = POWER * 1000;
-	return POWER;
-}
-
-// <Ваты> ---> <Мегаваты> //
-double Vat2mVat(double POWER) {
-	POWER = POWER / 1000000;
-	return POWER;
-}
-
-// <Мегаваты> ---> <Ваты> //
-double mVat2Vat(double POWER) {
-	POWER = POWER * 1000000;
-	return POWER;
-}
-
-// <Киловаты> ---> <Мегаваты> //
-double kVat2mVat(double POWER) {
-	POWER = POWER / 1000;
-	return POWER;
-}
-
-// <Мегаваты> ---> <Киловаты> //
-double mVat2kVat(double POWER) {
-	POWER = POWER * 1000;
-	return POWER;
-}
-
-// <Килограмм-сила-метр в секунду> ---> <Ваты> //
-double kgSilaMvS2Vat(double POWER) {
-	POWER = POWER * 9.81;
-	return POWER;
-}
-
-// <Ваты> ---> <Килограмм-сила-метр в секунду> //
-double Vat2kgSilaMvS(double POWER) {
-	POWER = POWER / 9.81;
-	return POWER;
-}
-
-// <Килограмм-сила-метр в секунду> ---> <Киловаты> //
-double kgSilaMvS2kVat(double POWER) {
-	POWER = (POWER * 9.81) / 1000;
-	return POWER;
-}
-
-// <Киловаты> ---> <Килограмм-сила-метр в секунду> //
-double kVat2kgSilaMvS(double POWER) {
-	POWER = (POWER * 1000) / 9.81;
-	return POWER;
-}
-	
-// <Килограмм-сила-метр в секунду> ---> <Мегаваты> //
-double kgSilaMvS2mVat(double POWER) {
-	POWER = (POWER * 9.81) / 1000000;
-	return POWER;
-}
-
-// <Мегаваты> ---> <Килограмм-сила-метр в секунду> //
-double mVat2kgSilaMvS(double POWER) {
-	POWER = (POWER * 1000000) / 9.81;
-	return POWER;
-}
-
-// <Ваты> ---> <Эрг в секунду> //
-double Vat2ErgS(double POWER) {
-	POWER = POWER  * 10000000;
-	return POWER;
-}
-
-// <Эрг в секунду> ---> <Ваты> //
-double ErgS2Vat(double POWER) {
-	POWER = POWER / 10000000;
-	return POWER;
-}
-
-// <Киловаты> ---> <Эрг в секунду> //
-double kVat2ErgS(double POWER) {
-	POWER = POWER  * 10000000000;
-	return POWER;
-}
-
-// <Эрг в секунду> ---> <Киловаты> //
-double ErgS2kVat(double POWER) {
-	POWER = POWER / 10000000000;
-	return POWER;
-}
-
-// <Мегаваты> ---> <Эрг в секунду> //
-double mVat2ErgS(double POWER) {
-	POWER = POWER  * 10000000000000;
-	return POWER;
-}
-
-// <Эрг в секунду> ---> <Мегаваты> //
-double ErgS2mVat(double POWER) {
-	POWER = POWER / 10000000000000;
-	return POWER;
-}
-
-// <Килограмм-сила-метр в секунду> ---> <Эрг в секунду> //
-double kgSilaMvS2ErgS(double POWER) {
-	POWER = POWER  * 9.81 * 10000000;
-	return POWER;
-}
-
-// <Эрг в секунду> ---> <Килограмм-сила-метр в секунду> //
-double ErgS2kgSilaMvS(double POWER) {
-	POWER = POWER / (9.81 * 10000000);
-	return POWER;
-}
-
-// <Ваты> ---> <Лошадиные силы(метрические)> //
-double Vat2HorsePowMetr(double POWER) {
-	POWER = POWER  / 735.5;
-	return POWER;
-}
-
-// <Лошадиные силы(метрические)> ---> <Ваты> //
-double HorsePowMetr2Vat(double POWER) {
-	POWER = POWER * 735.5;
-	return POWER;
-}
-
-// <Киловаты> ---> <Лошадиные силы(метрические)> //
-double kVat2HorsePowMetr(double POWER) {
-	POWER = (POWER  * 1000) / 735.5;
-	return POWER;
-}
-
-// <Лошадиные силы(метрические)> ---> <Киловаты> //
-double HorsePowMetr2kVat(double POWER) {
-	POWER = (POWER * 735.5) / 1000;
-	return POWER;
-}
-
-// <Мегаваты> ---> <Лошадиные силы(метрические)> //
-double mVat2HorsePowMetr(double POWER) {
-	POWER = (POWER  * 1000000) / 735.5;
-	return POWER;
-}
-
-// <Лошадиные силы(метрические)> ---> <Мегаваты> //
-double HorsePowMetr2mVat(double POWER) {
-	POWER = (POWER * 735.5) / 1000000;
-	return POWER;
-}
-
-// <Килограмм-сила-метр в секунду> ---> <Лошадиные силы(метрические)> //
-double kgSilaMvS2HorsePowMetr(double POWER) {
-	POWER = POWER / 75;
-	return POWER;
-}
-
-// <Лошадиные силы(метрические)> ---> <Килограмм-сила-метр в секунду> //
-double HorsePowMetr2kgSilaMvS(double POWER) {
-	POWER = POWER * 75;
-	return POWER;
-}
-
-// <Эрг в секунду> ---> <Лошадиные силы(метрические)> //
-double ErgS2HorsePowMetr(double POWER) {
-	POWER = POWER / (7.355 * 1000000000);
-	return POWER;
-}
-
-// <Лошадиные силы(метрические)> ---> <Эрг в секунду> //
-double HorsePowMetr2ErgS(double POWER) {
-	POWER = POWER * 7.355 * 1000000000;
-	return POWER;
-}
-
-// <Ваты> ---> <Лошадиные силы(английские)> //
-double Vat2HorsePowEng(double POWER) {
-	POWER = POWER / 745.7;
-	return POWER;
-}
-
-// <Лошадиные силы(английские)> ---> <Ваты> //
-double HorsePowEng2Vat(double POWER) {
-	POWER = POWER * 745.7;
-	return POWER;
-}
-
-// <Киловаты> ---> <Лошадиные силы(английские)> //
-double kVat2HorsePowEng(double POWER) {
-	POWER = (POWER * 1000) / 745.7;
-	return POWER;
-}
-
-// <Лошадиные силы(английские)> ---> <Киловаты> //
-double HorsePowEng2kVat(double POWER) {
-	POWER = (POWER * 745.7) / 1000;
-	return POWER;
-}
-
-// <Мегаваты> ---> <Лошадиные силы(английские)> //
-double mVat2HorsePowEng(double POWER) {
-	POWER = (POWER * 1000000) / 745.7;
-	return POWER;
-}
-
-// <Лошадиные силы(английские)> ---> <Мегаваты> //
-double HorsePowEng2mVat(double POWER) {
-	POWER = (POWER * 745.7) / 1000000;
-	return POWER;
-}
-
-// <Килограмм-сила-метр в секунду> ---> <Лошадиные силы(английские)> //
-double kgSilaMvS2HorsePowEng(double POWER) {
-	POWER = POWER / 76.04;
-	return POWER;
-}
-
-// <Лошадиные силы(английские)> ---> <Килограмм-сила-метр в секунду> //
-double HorsePowEng2kgSilaMvS(double POWER) {
-	POWER = POWER * 76.04;
-	return POWER;
-}
-
-// <Эрг в секунду> ---> <Лошадиные силы(английские)> //
-double ErgS2HorsePowEng(double POWER) {
-	POWER = POWER / (7.457 * 1000000000);
-	return POWER;
-}
-
-// <Лошадиные силы(английские)> ---> <Эрг в секунду> //
-double HorsePowEng2ErgS(double POWER) {
-	POWER = POWER * 7.457 * 1000000000;
-	return POWER;
-}
-
-// <Лошадиные силы(метрические)> ---> <Лошадиные силы(английские)> //
-double HorsePowMetr2HorsePowEng(double POWER) {
-	POWER = POWER / 1.014;
-	return POWER;
-}
-
-// <Лошадиные силы(английские)> ---> <Лошадиные силы(метрические)> //
-double HorsePowEng2HorsePowMetr(double POWER) {
-	POWER = POWER * 1.014;
-	return POWER;
-}
-
-// <Ваты> ---> <Милливаты> //
-double Vat2MlVat(double POWER) {
-	POWER = POWER * 1000;
-	return POWER;
-}
-
-// <Милливаты> ---> <Ваты> //
-double MlVat2Vat(double POWER) {
-	POWER = POWER / 1000;
-	return POWER;
-}
-
-// <Киловаты> ---> <Милливаты> //
-double kVat2MlVat(double POWER) {
-	POWER = POWER * 1000000;
-	return POWER;
-}
-
-// <Милливаты> ---> <Киловаты> //
-double MlVat2kVat(double POWER) {
-	POWER = POWER / 1000000;
-	return POWER;
-}
-
-// <Мегаваты> ---> <Милливаты> //
-double mVat2MlVat(double POWER) {
-	POWER = POWER * 1000000000;
-	return POWER;
-}
-
-// <Милливаты> ---> <Мегаваты> //
-double MlVat2mVat(double POWER) {
-	POWER = POWER / 1000000000;
-	return POWER;
-}
-
-// <Милливаты> ---> <Килограмм-сила-метр в секунду> //
-double MlVat2kgSilaMvS(double POWER) {
-	POWER = POWER / 9.81 / 1000;
-	return POWER;
-}
-
-// <Килограмм-сила-метр в секунду> ---> <Милливаты> //
-double kgSilaMvS2MlVat(double POWER) {
-	POWER = (POWER * 9.81) / 1000;
-	return POWER;
-}
-
-// <Милливаты> ---> <Эрг в секунду> //
-double MlVat2ErgS(double POWER) {
-	POWER = POWER  * 10000;
-	return POWER;
-}
-
-// <Эрг в секунду> ---> <Милливаты> //
-double ErgS2MlVat(double POWER) {
-	POWER = POWER / 10000;
-	return POWER;
-}
-
-// <Милливаты> ---> <Лошадиные силы(метрические)> //
-double MlVat2HorsePowMetr(double POWER) {
-	POWER = POWER  / 735.5 / 1000;
-	return POWER;
-}
-
-// <Лошадиные силы(метрические)> ---> <Милливаты> //
-double HorsePowMetr2MlVat(double POWER) {
-	POWER = (POWER * 735.5) / 1000;
-	return POWER;
-}
-
-// <Милливаты> ---> <Лошадиные силы(английские)> //
-double MlVat2HorsePowEng(double POWER) {
-	POWER = POWER / 745.7 / 1000;
-	return POWER;
-}
-
-// <Лошадиные силы(английские)> ---> <Милливаты> //
-double HorsePowEng2MlVat(double POWER) {
-	POWER = (POWER * 745.7) / 1000;
-	return POWER;
-}
-
-// <Милливаты> ---> <Микроваты> //
-double MlVat2MkVat(double POWER) {
-	POWER = POWER * 1000;
-	return POWER;
-}
-
-// <Микроваты> ---> <Милливаты> //
-double MkVat2MlVat(double POWER) {
-	POWER = POWER / 1000;
-	return POWER;
-}
-
-// <Ваты> ---> <Микроваты> //
-double Vat2MkVat(double POWER) {
-	POWER = POWER * 1000000;
-	return POWER;
-}
-
-// <Микроваты> ---> <Ваты> //
-double MkVat2Vat(double POWER) {
-	POWER = POWER / 1000000;
-	return POWER;
-}
-
-// <Киловаты> ---> <Микроваты> //
-double kVat2MkVat(double POWER) {
-	POWER = POWER * 1000000000;
-	return POWER;
-}
-
-// <Микроваты> ---> <Киловаты> //
-double MkVat2kVat(double POWER) {
-	POWER = POWER / 1000000000;
-	return POWER;
-}
-
-// <Мегаваты> ---> <Микроваты> //
-double mVat2MkVat(double POWER) {
-	POWER = POWER * 1000000000000;
-	return POWER;
-}
-
-// <Микроваты> ---> <Мегаваты> //
-double MkVat2mVat(double POWER) {
-	POWER = POWER / 1000000000000;
-	return POWER;
-}
-
-// <Микроваты> ---> <Килограмм-сила-метр в секунду> //
-double MkVat2kgSilaMvS(double POWER) {
-	POWER = POWER / 9.81 / 1000000;
-	return POWER;
-}
-
-// <Килограмм-сила-метр в секунду> ---> <Микроваты> //
-double kgSilaMvS2MkVat(double POWER) {
-	POWER = (POWER * 9.81) / 1000000;
-	return POWER;
-}
-
-// <Микроваты> ---> <Эрг в секунду> //
-double MkVat2ErgS(double POWER) {
-	POWER = POWER  * 10;
-	return POWER;
-}
-
-// <Эрг в секунду> ---> <Микроваты> //
-double ErgS2MkVat(double POWER) {
-	POWER = POWER / 10;
-	return POWER;
-}
-
-// <Микроваты> ---> <Лошадиные силы(метрические)> //
-double MkVat2HorsePowMetr(double POWER) {
-	POWER = POWER  / 735.5 / 1000000;
-	return POWER;
-}
-
-// <Лошадиные силы(метрические)> ---> <Микроваты> //
-double HorsePowMetr2MkVat(double POWER) {
-	POWER = (POWER * 735.5) / 1000000;
-	return POWER;
-}
-
-// <Микроваты> ---> <Лошадиные силы(английские)> //
-double MkVat2HorsePowEng(double POWER) {
-	POWER = POWER / 745.7 / 1000000;
-	return POWER;
-}
-
-// <Лошадиные силы(английские)> ---> <Микроваты> //
-double HorsePowEng2MkVat(double POWER) {
-	POWER = (POWER * 745.7) / 1000000;
-	return POWER;
+#include <math.h>
+#include"../Headers/total.h" /* Символ '../' - escape-символ - используется для перехода на один уровень выше;
+                                Этот символ используется столько раз, на сколько уровней нужно подняться; */
+
+double selectionswitchPower(double MEASUREMENT)
+{
+    FunctionsParameters key;
+
+    printf("Выберите величину, из которой Вы хотите произвести конвертирование:\n"
+    	   "1 - Микроват;\n"
+           "2 - Милливат;\n"
+           "3 - Ват;\n"
+           "4 - Киловат;\n"
+           "5 - Мегават;\n"
+           "6 - Килограмм-сил-метров в секунду;\n"
+           "7 - Эрг в секунду;\n"
+           "8 - Лошадиных сил(метрических);\n"
+           "9 - Лошадиных сил(английских);\n\t");
+    key.input_choice = parsing_id(key.in);
+    //scanf("%d", &key.input_choice);
+    while(key.input_choice >= 10 || key.input_choice == 0) {
+        fprintf(stderr, "\nВы выбрали величину, которой нет в списке доступных величин. \nПожалуйста, сделайте корректный выбор\n\t");
+        scanf("%d", &key.input_choice);
+    }
+
+    printf("Выберите величину, в которую Вы хотите конвертировать Вашу величину.\n"
+           "Не нужно выбирать ту же величину, что Вы выбрали в качестве конвертируемой:\n"
+    	   "1 - Микроват;\n"
+           "2 - Милливат;\n"
+           "3 - Ват;\n"
+           "4 - Киловат;\n"
+           "5 - Мегават;\n"
+           "6 - Килограмм-сил-метров в секунду;\n"
+           "7 - Эрг в секунду;\n"
+           "8 - Лошадиных сил(метрических);\n"
+           "9 - Лошадиных сил(английских);\n\t");
+    key.output_choice = parsing_id(key.out);
+    //scanf("%d", &key.output_choice);
+    while(key.output_choice == key.input_choice || key.input_choice >= 10 || key.input_choice == 0 )  {
+        fprintf(stderr, "\nВы выбрали величину, которой нет в списке доступных величин, \nили Вы пытаетесь конвертировать "
+                "одну и ту же величину. \nПожалуйста, сделайте корректный выбор\n\t");
+        scanf("%d", &key.output_choice);
+    }
+
+    key.id = key.input_choice * 10 + key.output_choice;
+
+    switch(key.id) {
+        case 12:
+            key.gate = Vat2MlVat(MkVat2Vat(MEASUREMENT));
+            break;
+        case 13:
+            key.gate = MkVat2Vat(MEASUREMENT);
+            break;
+        case 14:
+            key.gate = Vat2kVat(MkVat2Vat(MEASUREMENT));
+            break;
+        case 15:
+            key.gate = Vat2mVat(MkVat2Vat(MEASUREMENT));
+            break;
+        case 16:
+            key.gate = Vat2kgSilaMvS(MkVat2Vat(MEASUREMENT));
+            break;
+        case 17:
+            key.gate = Vat2ErgS(MkVat2Vat(MEASUREMENT));
+            break;
+        case 18:
+            key.gate = Vat2HorsePowMetr(MkVat2Vat(MEASUREMENT));
+            break;
+        case 19:
+            key.gate = Vat2HorsePowEng(MkVat2Vat(MEASUREMENT));
+            break;
+        case 21:
+            key.gate = Vat2MkVat(MlVat2Vat(MEASUREMENT));
+            break;
+        case 23:
+            key.gate = MlVat2Vat(MEASUREMENT);
+            break;
+        case 24:
+            key.gate = Vat2kVat(MlVat2Vat(MEASUREMENT));
+            break;
+        case 25:
+            key.gate = Vat2mVat(MlVat2Vat(MEASUREMENT));
+            break;
+        case 26:
+            key.gate = Vat2kgSilaMvS(MlVat2Vat(MEASUREMENT));
+            break;
+        case 27:
+            key.gate = Vat2ErgS(MlVat2Vat(MEASUREMENT));
+            break;
+        case 28:
+            key.gate = Vat2HorsePowMetr(MlVat2Vat(MEASUREMENT));
+            break;
+        case 29:
+            key.gate = Vat2HorsePowEng(MlVat2Vat(MEASUREMENT));
+            break;
+        case 31:
+            key.gate = Vat2MkVat(MEASUREMENT);
+            break;
+        case 32:
+            key.gate = Vat2MlVat(MEASUREMENT);
+            break;
+        case 34:
+            key.gate = Vat2kVat(MEASUREMENT);
+            break;
+        case 35:
+            key.gate = Vat2mVat(MEASUREMENT);
+            break;
+        case 36:
+            key.gate = Vat2kgSilaMvS(MEASUREMENT);
+            break;
+        case 37:
+            key.gate = Vat2ErgS(MEASUREMENT);
+            break;
+        case 38:
+            key.gate = Vat2HorsePowMetr(MEASUREMENT);
+            break;
+        case 39:
+            key.gate = Vat2HorsePowEng(MEASUREMENT);
+            break;
+        case 41:
+            key.gate = Vat2MkVat(kVat2Vat(MEASUREMENT));
+            break;
+        case 42:
+            key.gate = Vat2MlVat(kVat2Vat(MEASUREMENT));
+            break;
+        case 43:
+            key.gate = kVat2Vat(MEASUREMENT);
+            break;
+        case 45:
+            key.gate = Vat2mVat(kVat2Vat(MEASUREMENT));
+            break;
+        case 46:
+            key.gate = Vat2kgSilaMvS(kVat2Vat(MEASUREMENT));
+            break;
+        case 47:
+            key.gate = Vat2ErgS(kVat2Vat(MEASUREMENT));
+            break;
+        case 48:
+            key.gate = Vat2HorsePowMetr(kVat2Vat(MEASUREMENT));
+            break;
+        case 49:
+            key.gate = Vat2HorsePowEng(kVat2Vat(MEASUREMENT));
+            break;
+        case 51:
+            key.gate = Vat2MkVat(mVat2Vat(MEASUREMENT));
+            break;
+        case 52:
+            key.gate = Vat2MlVat(mVat2Vat(MEASUREMENT));
+            break;
+        case 53:
+            key.gate = mVat2Vat(MEASUREMENT);
+            break;
+        case 54:
+            key.gate = Vat2kVat(mVat2Vat(MEASUREMENT));
+            break;
+        case 56:
+            key.gate = Vat2kgSilaMvS(mVat2Vat(MEASUREMENT));
+            break;
+        case 57:
+            key.gate = Vat2ErgS(mVat2Vat(MEASUREMENT));
+            break;
+        case 58:
+            key.gate = Vat2HorsePowMetr(mVat2Vat(MEASUREMENT));
+            break;
+        case 59:
+            key.gate = Vat2HorsePowEng(mVat2Vat(MEASUREMENT));
+            break;
+        case 61:
+            key.gate = Vat2MkVat(kgSilaMvS2Vat(MEASUREMENT));
+            break;
+        case 62:
+            key.gate = Vat2MlVat(kgSilaMvS2Vat(MEASUREMENT));
+            break;
+        case 63:
+            key.gate = kgSilaMvS2Vat(MEASUREMENT);
+            break;
+        case 64:
+            key.gate = Vat2kVat(kgSilaMvS2Vat(MEASUREMENT));
+            break;
+        case 65:
+            key.gate = Vat2mVat(kgSilaMvS2Vat(MEASUREMENT));
+            break;
+        case 67:
+            key.gate = Vat2ErgS(kgSilaMvS2Vat(MEASUREMENT));
+            break;
+        case 68:
+            key.gate = Vat2HorsePowMetr(kgSilaMvS2Vat(MEASUREMENT));
+            break;
+        case 69:
+            key.gate = Vat2HorsePowEng(kgSilaMvS2Vat(MEASUREMENT));
+            break;
+        case 71:
+            key.gate = Vat2MkVat(ErgS2Vat(MEASUREMENT));
+            break;
+        case 72:
+            key.gate = Vat2MlVat(ErgS2Vat(MEASUREMENT));
+            break;
+        case 73:
+            key.gate = ErgS2Vat(MEASUREMENT);
+            break;
+        case 74:
+            key.gate = Vat2kVat(ErgS2Vat(MEASUREMENT));
+            break;
+        case 75:
+            key.gate = Vat2mVat(ErgS2Vat(MEASUREMENT));
+            break;
+        case 76:
+            key.gate = Vat2kgSilaMvS(ErgS2Vat(MEASUREMENT));
+            break;
+        case 78:
+            key.gate = Vat2HorsePowMetr(ErgS2Vat(MEASUREMENT));
+            break;
+        case 79:
+            key.gate = Vat2HorsePowEng(ErgS2Vat(MEASUREMENT));
+            break;
+        case 81:
+            key.gate = Vat2MkVat(HorsePowMetr2Vat(MEASUREMENT));
+            break;
+        case 82:
+            key.gate = Vat2MlVat(HorsePowMetr2Vat(MEASUREMENT));
+            break;
+        case 83:
+            key.gate = HorsePowMetr2Vat(MEASUREMENT);
+            break;
+        case 84:
+            key.gate = Vat2kVat(HorsePowMetr2Vat(MEASUREMENT));
+            break;
+        case 85:
+            key.gate = Vat2mVat(HorsePowMetr2Vat(MEASUREMENT));
+            break;
+        case 86:
+            key.gate = Vat2kgSilaMvS(HorsePowMetr2Vat(MEASUREMENT));
+            break;
+        case 87:
+            key.gate = Vat2ErgS(HorsePowMetr2Vat(MEASUREMENT));
+            break;
+        case 89:
+            key.gate = Vat2HorsePowEng(HorsePowMetr2Vat(MEASUREMENT));
+            break;
+        case 91:
+            key.gate = Vat2MkVat(HorsePowEng2Vat(MEASUREMENT));
+            break;
+        case 92:
+            key.gate = Vat2MlVat(HorsePowEng2Vat(MEASUREMENT));
+            break;
+        case 93:
+            key.gate = HorsePowEng2Vat(MEASUREMENT);
+            break;
+        case 94:
+            key.gate = Vat2kVat(HorsePowEng2Vat(MEASUREMENT));
+            break;
+        case 95:
+            key.gate = Vat2mVat(HorsePowEng2Vat(MEASUREMENT));
+            break;
+        case 96:
+            key.gate = Vat2kgSilaMvS(HorsePowEng2Vat(MEASUREMENT));
+            break;
+        case 97:
+            key.gate = Vat2ErgS(HorsePowEng2Vat(MEASUREMENT));
+            break;
+        case 98:
+            key.gate = Vat2HorsePowMetr(HorsePowEng2Vat(MEASUREMENT));
+            break;
+        default:
+            printf("Не введено значение конвертируемой величины, "
+                   "или Вы пытаетесь конвертировать в величину, которую уже преобразуете\n");
+            break;
+    }
+
+    return key.gate;
 }
