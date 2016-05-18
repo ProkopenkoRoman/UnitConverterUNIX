28cd6136919020b548f2820d63788809862a7de6
ArsenyLisenko9651 <Lisenko.A95@yandex.ru>
2016-05-18 19:11:19 +0600

    Update selectionswitchTemperature.c

--- Project/Switches/selectionswitchTemperature.c
+++ Project/Switches/selectionswitchTemperature.c
@@ -1,215 +1,230 @@
 #include <stdio.h>
 #include <math.h>
-#include "../Headers/total.h" /* Ñèìâîë '../' - escape-ñèìâîë - èñïîëüçóåòñÿ äëÿ ïåðåõîäà íà îäèí óðîâåíü âûøå;
-                                Ýòîò ñèìâîë èñïîëüçóåòñÿ ñòîëüêî ðàç, íà ñêîëüêî óðîâíåé íóæíî ïîäíÿòüñÿ; */
+#include"../Headers/total.h" /* Символ '../' - escape-символ - используется для перехода на один уровень выше;
+                                Этот символ используется столько раз, на сколько уровней нужно подняться; */
+
 
 double selectionswitchTemperature(double MEASUREMENT)
 {
     FunctionsParameters key;
 
     printf("Выберите величину, из которой Вы хотите произвести конвертирование:\n"
-           "1 - Градус Делиля;\n"
-           "2 - Градус Кельвина;\n"
-           "3 - Градус Ньтона;\n"
-           "4 - Градус Ранкина;\n"
-           "5 - Градус Реомюра;\n"
-           "6 - Градус Рёмера;\n"
-           "7 - Градус Цельсия;\n"
-           "8 - Градус Фаренгейта;\n \t");
+           "1 - Цельсий;\n"
+           "2 - Фаренгейт;\n"
+           "3 - Кельвин;\n"
+           "4 - Реомюра;\n"
+           "5 - Ранкина;\n"
+           "6 - Ньютона;\n"
+           "7 - Рёмера;\n"
+           "8 - Делисле;\n \t");
     //scanf("%d", &key.input_choice);
     key.input_choice = parsing_id(key.in);
-    while(key.input_choice >= 9 || key.input_choice == 0) {
+    while(key.input_choice > 8|| key.input_choice == 0) {
         fprintf(stderr, "\nВы выбрали величину, которой нет в списке доступных величин. \nПожалуйста, сделайте корректный выбор\n\t");
         scanf("%d", &key.input_choice);
     }
 
-    printf("Выберите величину, из которой Вы хотите произвести конвертирование:\n"
-           "1 - Градус Делиля;\n"
-           "2 - Градус Кельвина;\n"
-           "3 - Градус Ньтона;\n"
-           "4 - Градус Ранкина;\n"
-           "5 - Градус Реомюра;\n"
-           "6 - Градус Рёмера;\n"
-           "7 - Градус Цельсия;\n"
-           "8 - Градус Фаренгейта;\n \t");
+    printf("Выберите величину, в которую Вы хотите конвертировать Вашу величину.\n"
+           "Не нужно выбирать ту же величину, что Вы выбрали в качестве конвертируемой:\n"
+           "1 - Цельсий;\n"
+           "2 - Фаренгейт;\n"
+           "3 - Кельвин;\n"
+           "4 - Реомюра;\n"
+           "5 - Ранкина;\n"
+           "6 - Ньютона;\n"
+           "7 - Рёмера;\n"
+           "8 - Делисле;\n \t");
     //scanf("%d", &key.output_choice);
     key.output_choice = parsing_id(key.out);
-    while(key.output_choice == key.input_choice || key.output_choice >= 9 || key.input_choice == 0)  {
+    while(key.output_choice == key.input_choice || key.input_choice > 8 || key.input_choice == 0 )  {
         fprintf(stderr, "\nВы выбрали величину, которой нет в списке доступных величин, \nили Вы пытаетесь конвертировать "
                 "одну и ту же величину. \nПожалуйста, сделайте корректный выбор\n\t");
         scanf("%d", &key.output_choice);
     }
 
-    key.id = key.input_choice * 10 + key.output_choice;
+    if(input_choice < 10 && output_choice < 10)
+		key.id = key.input_choice * 10 + key.output_choice;
+	if(input_choice == 10 && output_choice <= 10)
+		key.id = key.input_choice * 100 + key.output_choice;
+	if(input_choice < 10 && output_choice == 10)
+		key.id = key.input_choice * 100 + key.output_choice;
 
     switch(key.id) {
+        //Цельсий
         case 12:
-            key.gate = DelislesTOKelvins(MEASUREMENT);
+            key.gate = Kelvins_TO_Fahrenheits(Celsius_TO_Kelvins(MEASUREMENT));
             break;
         case 13:
-            key.gate = DelislesTONewtons(MEASUREMENT);
+            key.gate = Celsius_TO_Kelvins(MEASUREMENT);
             break;
         case 14:
-            key.gate = DelislesTORankines(MEASUREMENT);
+            key.gate = Kelvins_TO_Reaumurs(Celsius_TO_Kelvins(MEASUREMENT));
             break;
         case 15:
-            key.gate = DelislesTOReaumurs(MEASUREMENT);
+            key.gate = Kelvins_TO_Rankines(Celsius_TO_Kelvins(MEASUREMENT));
             break;
         case 16:
-            key.gate = DelislesTORomers(MEASUREMENT);
+            key.gate = Kelvins_TO_Newtons(Celsius_TO_Kelvins(MEASUREMENT));
             break;
         case 17:
-            key.gate = DelislesTOCelsius(MEASUREMENT);
+            key.gate = Kelvins_TO_Romers(Celsius_TO_Kelvins(MEASUREMENT));
             break;
         case 18:
-            key.gate = DelislesTOFahrenheits(MEASUREMENT);
+            key.gate = Kelvins_TO_Delisles(Celsius_TO_Kelvins(MEASUREMENT));
+            break;
+        //Фаренгейт
+        case 23:
+            key.gate = Fahrenheits_TO_Kelvins(MEASUREMENT);
             break;
         case 21:
-            key.gate = KelvinsTODelisles(MEASUREMENT);
-            break;
-        case 23:
-            key.gate = KelvinsTONewtons(MEASUREMENT);
+            key.gate = Kelvins_TO_Celsiuss(Fahrenheits_TO_Kelvins(MEASUREMENT));
             break;
         case 24:
-            key.gate = KelvinsTORankines(MEASUREMENT);
+            key.gate = Kelvins_TO_Reaumurs(Fahrenheits_TO_Kelvins(MEASUREMENT));
             break;
         case 25:
-            key.gate = KelvinsTOReaumurs(MEASUREMENT);
+            key.gate = Kelvins_TO_Rankines(Fahrenheits_TO_Kelvins(MEASUREMENT));
             break;
         case 26:
-            key.gate = KelvinsTORomers(MEASUREMENT);
+            key.gate = Kelvins_TO_Newtons(Fahrenheits_TO_Kelvins(MEASUREMENT));
             break;
         case 27:
-            key.gate = KelvinsTOCelsius(MEASUREMENT);
+            key.gate = Kelvins_TO_Romers(Fahrenheits_TO_Kelvins(MEASUREMENT));
             break;
         case 28:
-            key.gate = KelvinsTOFahrenheits(MEASUREMENT);
+            key.gate = Kelvins_TO_Delisles(Fahrenheits_TO_Kelvins(MEASUREMENT));
+            break;
+        //Кельвин
+        case 32:
+            key.gate = Kelvins_TO_Fahrenheits(MEASUREMENT);
             break;
         case 31:
-            key.gate = NewtonsTODelisles(MEASUREMENT);
-            break;
-        case 32:
-            key.gate = NewtonsTOKelvins(MEASUREMENT);
+            key.gate = Kelvins_TO_Celsius(MEASUREMENT);
             break;
         case 34:
-            key.gate = NewtonsTORankines(MEASUREMENT);
+            key.gate = Kelvins_TO_Reaumurs(MEASUREMENT);
             break;
         case 35:
-            key.gate = NewtonsTOReaumurs(MEASUREMENT);
+            key.gate = Kelvins_TO_Rankines(MEASUREMENT);
             break;
         case 36:
-            key.gate = NewtonsTORomers(MEASUREMENT);
+            key.gate = Kelvins_TO_Newtons(MEASUREMENT);
             break;
         case 37:
-            key.gate = NewtonsTOCelsius(MEASUREMENT);
+            key.gate = Kelvins_TO_Romers(MEASUREMENT);
             break;
         case 38:
-            key.gate = NewtonsTOFahrenheits(MEASUREMENT);
+            key.gate = Kelvins_TO_Delisles(MEASUREMENT);
             break;
-        case 41:
-            key.gate = RankinesTODelisles(MEASUREMENT);
+        //Реомюр
+        case 43:
+            key.gate = Reaumurs_TO_Kelvins(MEASUREMENT);
             break;
         case 42:
-            key.gate = RankinesTOKelvins(MEASUREMENT);
+            key.gate = Kelvins_TO_Fahrenheits(Reaumurs_TO_Kelvins(MEASUREMENT));
             break;
-        case 43:
-            key.gate = RankinesTONewtons(MEASUREMENT);
+        case 41:
+            key.gate = Kelvins_TO_Celsius(Reaumurs_TO_Kelvins(MEASUREMENT));
             break;
         case 45:
-            key.gate = RankinesTOReaumurs(MEASUREMENT);
+            key.gate = Kelvins_TO_Rankines(Reaumurs_TO_Kelvins(MEASUREMENT));
             break;
         case 46:
-            key.gate = RankinesTORomers(MEASUREMENT);
+            key.gate = Kelvins_TO_Newtons(Reaumurs_TO_Kelvins(MEASUREMENT));
             break;
         case 47:
-            key.gate = RankinesTOCelsius(MEASUREMENT);
+            key.gate = Kelvins_TO_Romers(Reaumurs_TO_Kelvins(MEASUREMENT));
             break;
         case 48:
-            key.gate = RankinesTOFahrenheits(MEASUREMENT);
+            key.gate = Kelvins_TO_Delisles(Reaumurs_TO_Kelvins(MEASUREMENT));
             break;
-        case 51:
-            key.gate = ReaumursTODelisles(MEASUREMENT);
-            break;
-        case 52:
-            key.gate = ReaumursTOKelvins(MEASUREMENT);
+        //Ранкин
+        case 54:
+            key.gate = Kelvins_TO_Reaumurs(Rankines_TO_Kelvins(MEASUREMENT));
             break;
         case 53:
-            key.gate = ReaumursTONewtons(MEASUREMENT);
+            key.gate = Rankines_TO_Kelvins(MEASUREMENT);
             break;
-        case 54:
-            key.gate = ReaumursTORankines(MEASUREMENT);
+        case 52:
+            key.gate = Kelvins_TO_Fahrenheits(Rankines_TO_Kelvins(MEASUREMENT));
+            break;
+        case 51:
+            key.gate = Kelvins_TO_Celsius(Rankines_TO_Kelvins(MEASUREMENT));
             break;
         case 56:
-            key.gate = ReaumursTORomers(MEASUREMENT);
+            key.gate = Kelvins_TO_Newtons(Rankines_TO_Kelvins(MEASUREMENT));
             break;
         case 57:
-            key.gate = ReaumursTOCelsius(MEASUREMENT);
+            key.gate = Kelvins_TO_Romers(Rankines_TO_Kelvins(MEASUREMENT));
             break;
         case 58:
-            key.gate = ReaumursTOFahrenheits(MEASUREMENT);
+            key.gate = Kelvins_TO_Delisles(Rankines_TO_Kelvins(MEASUREMENT));
             break;
-        case 61:
-            key.gate = RomersTODelisles(MEASUREMENT);
-            break;
-        case 62:
-            key.gate = RomersTOKelvins(MEASUREMENT);
-            break;
-        case 63:
-            key.gate = RomersTONewtons(MEASUREMENT);
+        //Ньютон
+        case 65:
+            key.gate = Kelvins_TO_Rankines(Newtons_TO_Kelvins(MEASUREMENT));
             break;
         case 64:
-            key.gate = RomersTORankines(MEASUREMENT);
+            key.gate = Kelvins_TO_Reaumurs(Newtons_TO_Kelvins(MEASUREMENT));
             break;
-        case 65:
-            key.gate = RomersTOReaumurs(MEASUREMENT);
+        case 63:
+            key.gate = Newtons_TO_Kelvins(MEASUREMENT);
+            break;
+        case 62:
+            key.gate = Kelvins_TO_Fahrenheits(Newtons_TO_Kelvins(MEASUREMENT));
+            break;
+        case 61:
+            key.gate = Kelvins_TO_Celsius(Newtons_TO_Kelvins(MEASUREMENT));
             break;
         case 67:
-            key.gate = RomersTOCelsius(MEASUREMENT);
+            key.gate = Kelvins_TO_Romers(Newtons_TO_Kelvins(MEASUREMENT));
             break;
         case 68:
-            key.gate = RomersTOFahrenheits(MEASUREMENT);
+            key.gate = Kelvins_TO_Delisles(Newtons_TO_Kelvins(MEASUREMENT));
             break;
-        case 71:
-            key.gate = CelsiusTODelisles(MEASUREMENT);
-            break;
-        case 72:
-            key.gate = CelsiusTOKelvins(MEASUREMENT);
-            break;
-        case 73:
-            key.gate = CelsiusTONewtons(MEASUREMENT);
-            break;
-        case 74:
-            key.gate = CelsiusTORankines(MEASUREMENT);
+        //Рёмер
+        case 76:
+            key.gate = Kelvins_TO_Newtons(Romers_TO_Kelvins(MEASUREMENT));
             break;
         case 75:
-            key.gate = CelsiusTOReaumurs(MEASUREMENT);
+            key.gate = Kelvins_TO_Rankines(Romers_TO_Kelvins(MEASUREMENT));
             break;
-        case 76:
-            key.gate = CelsiusTORomers(MEASUREMENT);
+        case 74:
+            key.gate = Kelvins_TO_Reaumurs(Romers_TO_Kelvins(MEASUREMENT));
+            break;
+        case 73:
+            key.gate = Romers_TO_Kelvins(MEASUREMENT);
+            break;
+        case 72:
+            key.gate = Kelvins_TO_Fahrenheits(Romers_TO_Kelvins(MEASUREMENT));
+            break;
+        case 71:
+            key.gate = Kelvins_TO_Celsius(Romers_TO_Kelvins(MEASUREMENT));
             break;
         case 78:
-            key.gate = CelsiusTOFahrenheits(MEASUREMENT);
+            key.gate = Kelvins_TO_Delisles(Romers_TO_Kelvins(MEASUREMENT));
             break;
-        case 81:
-            key.gate = FahrenheitsTODelisles(MEASUREMENT);
-            break;
-        case 82:
-            key.gate = FahrenheitsTOKelvins(MEASUREMENT);
-            break;
-        case 83:
-            key.gate = FahrenheitsTONewtons(MEASUREMENT);
-            break;
-        case 84:
-            key.gate = FahrenheitsTORankines(MEASUREMENT);
-            break;
-        case 85:
-            key.gate = FahrenheitsTOReaumurs(MEASUREMENT);
+        //Делисле
+        case 87:
+            key.gate = Kelvins_TO_Romers(Delisles_TO_Kelvins(MEASUREMENT));
             break;
         case 86:
-            key.gate = FahrenheitsTORomers(MEASUREMENT);
+            key.gate = Kelvins_TO_Newtons(Delisles_TO_Kelvins(MEASUREMENT));
             break;
-        case 87:
-            key.gate = FahrenheitsTOCelsius(MEASUREMENT);
+        case 85:
+            key.gate = Kelvins_TO_Rankines(Delisles_TO_Kelvins(MEASUREMENT));
+            break;
+        case 84:
+            key.gate = Kelvins_TO_Reaumurs(Delisles_TO_Kelvins(MEASUREMENT));
+            break;
+        case 83:
+            key.gate = Delisles_TO_Kelvins(MEASUREMENT);
+            break;
+        case 82:
+            key.gate = Kelvins_TO_Fahrenheits(Delisles_TO_Kelvins(MEASUREMENT));
+            break;
+        case 81:
+            key.gate = Kelvins_TO_Celsius(Delisles_TO_Kelvins(MEASUREMENT));
             break;
         default:
             printf("Не введено значение конвертируемой величины, "
