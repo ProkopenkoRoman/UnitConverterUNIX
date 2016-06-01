a424d8fe6828f34ad13e63f1cfaa8bcd32610bfb
PopovMihail <popov-mm@ya.ru>
2016-06-01 12:36:10 +0700

    Add files via upload

--- Project/test/ctect.c
+++ Project/test/ctect.c
@@ -1,7 +1,7 @@
 #include "ctest.h"
 #include "../Headers/total.h" 
 
-const float a = 5;
+const double a = 0.00000000000000000000001;
 
 CTEST(total, parsing_ID1) {    
     int  res = parsing_id("sd5gf48rtdv87");
@@ -26,3 +26,26 @@ CTEST(total, parsing_ID4) {
     int s = 5245;
     ASSERT_DBL_NEAR(s, res);
 }
+
+CTEST(total, parsing1) {    
+    double  res = parsing("dsf45s6d4f86df21");
+    double s = -1.111;
+    ASSERT_DBL_NEAR(s, res);
+}
+
+CTEST(total, parsing2) {    
+    double  res = parsing("87.48");
+    double s = 87.48;
+    ASSERT_DBL_NEAR(s, res);
+}
+/*
+CTEST(total, parsing3) {    
+    double  res = parsing("48,484");
+    double s = 0;
+    ASSERT_DBL_NEAR(s, res);
+}*//*
+CTEST(total, parsing2) {    
+    double  res = parsing("87.48");
+    double s = 87.48;
+    ASSERT_DBL_NEAR(s, res);
+}*/
